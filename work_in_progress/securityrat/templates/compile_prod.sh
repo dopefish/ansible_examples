da91f549669332d23d0edf9cf3206bd1dc6b80e2
Ryan Schulze <ryan@dopefish.de>
2016-09-09 09:33:06 -0500

    automatically change to directory the script is in

--- work_in_progress/securityrat/templates/compile_prod.sh
+++ work_in_progress/securityrat/templates/compile_prod.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo --set-home --user="{{ securityrat_local_user }}" mvn -Pprod -DskipTests package
+cd "${0%/*}"
+sudo --set-home --user="{{ securityrat_local_user }}" mvn -Pprod -DskipTests package && \
+ls -al target/*.war
